01756ec50af5acb33d0d417a643c69263a134ea1
Enrique Salas Rodriguez <enriquesaalas@gmail.com>
2017-11-18 08:57:15 -0600

    Add endpoint students returns all students

--- app/routes/student_routes.js
+++ app/routes/student_routes.js
@@ -16,15 +16,23 @@ module.exports = function(app, db){
         });
     });
 
-    app.get('students', (req,res) => {
-        db.collection('students').find();
+    //Trae a todos los estudiantes guardados en mongo lab
+    app.get('/students', (req,res) => {
+        db.collection('students').find({}).toArray((err, result) =>{
+            if(err){
+                res.send({ 'error': 'Error has ocurred'});
+            }else{
+                res.send(result);
+            }
+        });
     });
 
     app.post('/students', (req,res) =>{
+        var banned = (req.body.isBanned === "true");
         const student = { 
-            name: req.body.name,
-            description: req.body.description,
-            isBanned: req.body.isBanned
+            name: req.body.name.trim(),
+            description: req.body.description.trim(),
+            isBanned: banned
         }
         db.collection('students').insert(student, (err, result) => {
             if(err){
